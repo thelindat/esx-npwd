a614357266e1781058a5886bc728e3fbdb9fc48a
Linden <65407488+thelindat@users.noreply.github.com>
2021-11-29 19:59:23 +1100

    tweak: setup compatibility metatable and exports
    One day this will be tested with other versions of ESX

--- server.lua
+++ server.lua
@@ -5,29 +5,30 @@ ESX = {
 	GetPlayerFromId = ESX.GetPlayerFromId
 }
 
-local function getUniquePhoneNumber(xPlayer)
-	while true do
-		local phoneNumber = ('%s-%s-%s'):format(math.random(100,999), math.random(100,999), math.random(1000,9999))
-		local exists = exports.oxmysql:scalarSync('SELECT 1 FROM users WHERE phone_number = ?', { phoneNumber })
-
-		if not exists then
-			exports.oxmysql:update('UPDATE users SET phone_number = ? WHERE identifier = ?', { phoneNumber, xPlayer.identifier })
-			xPlayer.set('phoneNumber', phoneNumber)
-			return phoneNumber
-		end
-	end
+local function getUniquePhoneNumber(identifier)
+    local phoneNumber = exports.npwd:generatePhoneNumber()
+	exports.oxmysql:update('UPDATE users SET phone_number = ? WHERE identifier = ?', { phoneNumber, identifier })
+	return phoneNumber
 end
 
-AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
-	local variables = xPlayer.variables
-	local phoneNumber = (variables and variables.phoneNumber or xPlayer.get('phoneNumber')) or getUniquePhoneNumber(xPlayer)
+local rawget = rawget
+local variable_mt = {
+    __index = function(self, key)
+        return rawget(self, get) and self.get(key)
+    end
+}
 
-	TriggerEvent('npwd:newPlayer', {
+AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
+	local variables = xPlayer.variables or setmetatable(xPlayer, variable_mt)
+
+	local phoneNumber = variables.phoneNumber or getUniquePhoneNumber(xPlayer.identifier)
+
+	exports.npwd:newPlayer({
 		source = playerId,
 		identifier = xPlayer.identifier,
 		phoneNumber = phoneNumber,
-		firstname = variables and variables.firstName or xPlayer.get('firstName'),
-		lastname = variables and variables.lastName or xPlayer.get('lastName')
+		firstname = variables.firstName,
+		lastname = variables.lastName
 	})
 end)
 
@@ -35,37 +36,28 @@ AddEventHandler('esx:playerDropped', function(playerId)
 	TriggerEvent('npwd:unloadPlayer', playerId)
 end)
 
-AddEventHandler('onResourceStart', function(resource)
+local legacy
+
+AddEventHandler('onServerResourceStart', function(resource)
 	if resource == 'npwd' then
+        Wait(100)
 		local xPlayers = ESX.GetPlayers()
 		if next(xPlayers) then
 			-- Check for ESX Legacy
-			local legacy = type(xPlayers[1]) == 'table'
+			if not legacy then legacy = type(xPlayers[1]) == 'table' end
 
 			for i=1, #xPlayers do
 				-- Fallback to `GetPlayerFromId` if playerdata was not already returned
 				local xPlayer = legacy and xPlayers[i] or ESX.GetPlayerFromId(xPlayers[i])
-				local variables = xPlayer.variables
+				local variables = xPlayer.variables or setmetatable(xPlayer, variable_mt)
 
-				if variables then
-					-- Support for ESX 1.2+
-					TriggerEvent('npwd:newPlayer', {
-						source = xPlayer.source,
-						identifier = xPlayer.identifier,
-						phoneNumber = variables.phoneNumber,
-						firstname = variables.firstName,
-						lastname = variables.lastName
-					})
-				else
-					-- Support for ESX 1.1 with essentialmode
-					TriggerEvent('npwd:newPlayer', {
-						source = xPlayer.source,
-						identifier = xPlayer.identifier,
-						phoneNumber = xPlayer.get('phoneNumber'),
-						firstname = xPlayer.get('firstName'),
-						lastname = xPlayer.get('lastName')
-					})
-				end
+                exports.npwd:newPlayer({
+                    source = xPlayer.source,
+                    identifier = xPlayer.identifier,
+                    phoneNumber = variables.phoneNumber,
+                    firstname = variables.firstName,
+                    lastname = variables.lastName
+                })
 			end
 		end
 	end
