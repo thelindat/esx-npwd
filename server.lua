d2b70bc72be52be1eae684741089c3f7ffdb8f5e
Linden <65407488+thelindat@users.noreply.github.com>
2021-12-29 10:26:33 +1100

    fix: don't presume xPlayer's structure
    Most versions of ESX don't retrieve phone_number during xPlayer creation.
    
    At the loss of xPlayer.get('phoneNumber'), let NPWD handle the phone number entirely.

--- server.lua
+++ server.lua
@@ -5,33 +5,16 @@ ESX = {
 	GetPlayerFromId = ESX.GetPlayerFromId
 }
 
-local function getUniquePhoneNumber(identifier)
-	local phoneNumber = exports.npwd:generatePhoneNumber()
-	MySQL.Async.execute('UPDATE users SET phone_number = ? WHERE identifier = ?', { phoneNumber, identifier })
-	return phoneNumber
-end
-
-local rawget = rawget
-local variable_mt = {
-	__index = function(self, key)
-		return rawget(self, get) and self.get(key)
-	end
-}
-
 AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
 	if not xPlayer then
 		xPlayer = ESX.GetPlayerFromId(playerId)
 	end
 
-	local variables = xPlayer.variables or setmetatable(xPlayer, variable_mt)
-	local phoneNumber = variables.phoneNumber or getUniquePhoneNumber(xPlayer.identifier)
-
 	exports.npwd:newPlayer({
 		source = playerId,
 		identifier = xPlayer.identifier,
-		phoneNumber = phoneNumber,
-		firstname = variables.firstName,
-		lastname = variables.lastName
+		firstname = xPlayer.firstName or xPlayer.variables?.firstName or xPlayer.get('firstName'),
+		lastname = xPlayer.lastName or xPlayer.variables?.lastName or xPlayer.get('lastName')
 	})
 end)
 
@@ -50,14 +33,12 @@ AddEventHandler('onServerResourceStart', function(resource)
 			for i=1, #xPlayers do
 				-- Fallback to `GetPlayerFromId` if playerdata was not already returned
 				local xPlayer = isTable and xPlayers[i] or ESX.GetPlayerFromId(xPlayers[i])
-				local variables = xPlayer.variables or setmetatable(xPlayer, variable_mt)
 
 				exports.npwd:newPlayer({
 					source = xPlayer.source,
 					identifier = xPlayer.identifier,
-					phoneNumber = variables.phoneNumber,
-					firstname = variables.firstName,
-					lastname = variables.lastName
+					firstname = xPlayer.firstName or xPlayer.variables?.firstName or xPlayer.get('firstName'),
+					lastname = xPlayer.lastName or xPlayer.variables?.lastName or xPlayer.get('lastName')
 				})
 			end
 		end
