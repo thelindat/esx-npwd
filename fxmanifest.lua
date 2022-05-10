bb5c21d3f7988adb002f9297711295587af2f708
Linden <65407488+thelindat@users.noreply.github.com>
2022-05-10 20:24:01 +1000

    feat(client): phone visibility and disable

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
-description 'A simple bridge resource for ESX Compatibility for NPWD'
 
-server_only 'yes'
 use_fxv2_oal 'yes'
 lua54 'yes'
+
+client_script 'client.lua'
 
 server_scripts {
   '@mysql-async/lib/MySQL.lua',
